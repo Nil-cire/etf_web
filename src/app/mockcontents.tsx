c0bab975296badc25dfd04e25e23e0deab314304
徐子凡 <shiutzfan@xuzifandeMacBook-Air.local>
2024-04-14 13:51:59 +0800

    feat: add mock content

---
+++ src/app/mockcontents.tsx
@@ -0,0 +1,32 @@
+const world = [
+  {
+    type: "world",
+    name: "world",
+    id: "world",
+    point: 1,
+    trend: { point: 1, diff: "world" },
+  },
+];
+
+const materials = [
+  {
+    type: "material",
+    name: "material",
+    id: "material",
+    point: 1,
+    trend: { point: 1, diff: "material" },
+    thirty_day_trend: { point: 1, diff: "material" },
+  },
+];
+
+const etf = [
+  {
+    type: "etf",
+    name: "etf",
+    id: "etf",
+    point: 1,
+    trend: { point: 1, diff: "etf" },
+    estimate_interest: "etf",
+    interest: [{ month: "June", value: 1 }],
+  },
+];
